cd5fcfa65f9fab824c069a609e207d6a5806b551
David Smallbone Tizard <david@Davids-MacBook-Pro.local>
2015-03-30 09:12:28 +0200

    Initial Commit

---
+++ Locals/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Locals
+//
+//  Created by David Smallbone Tizard on 2015-03-30.
+//  Copyright (c) 2015 David Smallbone Tizard. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
